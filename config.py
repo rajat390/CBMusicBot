fd285a9176bfa6a34f9bf6906b8ca4050f38669e
THOMAS <gamingbadshah515@gmail.com>
2025-06-19 19:09:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,24 +8,25 @@ if os.path.exists("local.env"):
 load_dotenv()
 que = {}
 admins = {}
-SESSION_NAME = getenv("SESSION_NAME", "session")
-BOT_TOKEN = getenv("BOT_TOKEN")
-BOT_NAME = getenv("BOT_NAME", "ᴄʏʙᴇʀ ᴍᴜsɪᴄ ʙᴏᴛ")
-BG_IMAGE = getenv("BG_IMAGE", "https://telegra.ph/file/6790864f5fe27471bdc8d.png")
-THUMB_IMG = getenv("THUMB_IMG", "https://telegra.ph/file/e9a4d6655e5ddf51f9160.jpg")
-AUD_IMG = getenv("AUD_IMG", "https://telegra.ph/file/91034f175d41040d45b38.jpg")
-QUE_IMG = getenv("QUE_IMG", "https://telegra.ph/file/c8a0e9c544c5ea689caf9.jpg")
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
-BOT_USERNAME = getenv("BOT_USERNAME", "CyberMusikBot")
-ASSISTANT_NAME = getenv("ASSISTANT_NAME", "SaitamaHelper")
+SESSION_NAME = getenv("SESSION_NAME", "BQFxjfcAS8kiRAu9moRHy83V7iB5rhlUFwJhsI8UqpETC270mpv7N6KzLcyMXBRWT7poPHjrBIBRG3GR0ex4K2Uqys9x-ojEaOqcFJyHI9BHShT5Nv1IS5UJEp0PzJi_Q_5XS4IkKbE-vKyYsCrYgcfi5tWnytoijX8CIUEfmGNT5MeBtnWXsg5bN4xw-QKRfIk51PxCgpzb1Ew-mlGyGgAHUpmi2aErRn9_Ig4_a4sdOuygC2C0gFR7woeiqdDA7zkJ03Jm4jW4KjrqQOiVzd0L298pZZVQsWI59WJcfUkHqWw4wyDZAOGO1jqkdqK28Jx6x09NQzHYHXQUXJoAC8N12BM6cwAAAAE3MHqgAA")
+BOT_TOKEN = getenv("7729314076:AAHgtHs-5hhEWBR8Es3IyFQkIfs_VQ3VxoM")
+BOT_NAME = getenv("BOT_NAME", "BILLY MUSIC BOT")
+BG_IMAGE = getenv("BG_IMAGE", "https://graph.org/file/c34fc9d62d93032868f02-ffd4f1f6e48434a23a.jpg")
+THUMB_IMG = getenv("THUMB_IMG", "https://graph.org/file/c34fc9d62d93032868f02-ffd4f1f6e48434a23a.jpg")
+AUD_IMG = getenv("AUD_IMG", "https://graph.org/file/c34fc9d62d93032868f02-ffd4f1f6e48434a23a.jpg")
+QUE_IMG = getenv("QUE_IMG", "https://graph.org/file/c34fc9d62d93032868f02-ffd4f1f6e48434a23a.jpg")
+API_ID = int(getenv("24219127"))
+API_HASH = getenv("4ede46a38016f3574a35ab60055ce51a")
+BOT_USERNAME = getenv("BOT_USERNAME", "music_tit_bot")
+ASSISTANT_NAME = getenv("ASSISTANT_NAME", "Everything_is_okh")
 GROUP_SUPPORT = getenv("GROUP_SUPPORT", "CyberSupportGroup")
 UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "CyberMusicProject")
-OWNER_NAME = getenv("OWNER_NAME", "Badboyanim") # isi dengan username kamu tanpa simbol @
+OWNER_NAME = getenv("OWNER_NAME", "𝙅 𝙊 𝙃 𝙉 > 𝙒 𝙄 𝘾 𝙆") # isi dengan username kamu tanpa simbol @
 PMPERMIT = getenv("PMPERMIT", None)
-OWNER_ID = int(os.environ.get("OWNER_ID")) # fill with your id as the owner of the bot
-DATABASE_URL = os.environ.get("DATABASE_URL") # fill with your mongodb url
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL")) # make a private channel and get the channel id
+OWNER_ID = int(os.environ.get("7390298763")) # fill with your id as the owner of the bot
+DATABASE_URL = os.environ.get("mongodb+srv://rahucreate:rahucreate@cluster0.ohj4boi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+") # fill with your mongodb url
+LOG_CHANNEL = int(os.environ.get("-1002821231421")) # make a private channel and get the channel id
 BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False)) # just fill with True or False (optional)
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "60"))
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! .").split())
